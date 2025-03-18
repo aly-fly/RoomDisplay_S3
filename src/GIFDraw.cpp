a89f984fac67aa7485fb937c0d5b0e9071a566b6
Aljaz Ogrin <aljaz@ogrin.me>
2025-03-18 12:43:05 +0100

    Rendering GIF in a local buffer - does not work..

--- src/GIFDraw.cpp
+++ src/GIFDraw.cpp
@@ -4,16 +4,26 @@
 #include "display.h"
 #include "GIFDraw.h"
 
-// GIFDraw is called by AnimatedGIF library frame to screen
+AnimatedGIF gif;
+
+void initGIF(void)
+{
+  gif.begin(BIG_ENDIAN_PIXELS);
+}
+
+#ifdef GIF_DIRECT_RENDER_TO_LCD
+
+// DIRECT RENDER TO TFT DISPLAY
+// Code is from the example:
+// https://github.com/bitbank2/AnimatedGIF/tree/master/examples/TFT_eSPI_memory
 
 #ifdef GIF_USE_DMA
-uint16_t usTemp[2][GIF_BUFFER_SIZE]; // Global to support DMA use
+uint16_t usTemp[2][GIF_LINE_BUFFER_SIZE]; // Global to support DMA use
 #else
-uint16_t usTemp[1][GIF_BUFFER_SIZE]; // Global to support DMA use
+uint16_t usTemp[1][GIF_LINE_BUFFER_SIZE]; // Global to support DMA use
 #endif
 bool dmaBuf = 0;
 
-AnimatedGIF gif;
 int iOffX = 0, iOffY = 0;
 
 // Draw a line of image directly on the LCD
@@ -55,7 +65,7 @@ void GIFrenderToLCD_callback(GIFDRAW *pDraw)
     {
       c = ucTransparent - 1;
       d = &usTemp[0][0];
-      while (c != ucTransparent && s < pEnd && iCount < GIF_BUFFER_SIZE)
+      while (c != ucTransparent && s < pEnd && iCount < GIF_LINE_BUFFER_SIZE)
       {
         c = *s++;
         if (c == ucTransparent) // done, stop
@@ -94,11 +104,11 @@ void GIFrenderToLCD_callback(GIFDRAW *pDraw)
 
     // Unroll the first pass to boost DMA performance
     // Translate the 8-bit pixels through the RGB565 palette (already byte reversed)
-    if (iWidth <= GIF_BUFFER_SIZE)
+    if (iWidth <= GIF_LINE_BUFFER_SIZE)
       for (iCount = 0; iCount < iWidth; iCount++)
         usTemp[dmaBuf][iCount] = usPalette[*s++];
     else
-      for (iCount = 0; iCount < GIF_BUFFER_SIZE; iCount++)
+      for (iCount = 0; iCount < GIF_LINE_BUFFER_SIZE; iCount++)
         usTemp[dmaBuf][iCount] = usPalette[*s++];
 
 #ifdef GIF_USE_DMA // 71.6 fps (ST7796 84.5 fps)
@@ -116,11 +126,11 @@ void GIFrenderToLCD_callback(GIFDRAW *pDraw)
     while (iWidth > 0)
     {
       // Translate the 8-bit pixels through the RGB565 palette (already byte reversed)
-      if (iWidth <= GIF_BUFFER_SIZE)
+      if (iWidth <= GIF_LINE_BUFFER_SIZE)
         for (iCount = 0; iCount < iWidth; iCount++)
           usTemp[dmaBuf][iCount] = usPalette[*s++];
       else
-        for (iCount = 0; iCount < GIF_BUFFER_SIZE; iCount++)
+        for (iCount = 0; iCount < GIF_LINE_BUFFER_SIZE; iCount++)
           usTemp[dmaBuf][iCount] = usPalette[*s++];
 
 #ifdef GIF_USE_DMA
@@ -145,7 +155,7 @@ bool DisplayGIF(uint8_t *imageData, int imageSize, uint8_t numberRepetitons, int
   if (gif.open((uint8_t *)imageData, imageSize, GIFrenderToLCD_callback))
   {
     Serial.printf("Successfully opened GIF; Canvas size = %d x %d\n", gif.getCanvasWidth(), gif.getCanvasHeight());
-    if (positionX == -1)  // center on the display
+    if (positionX == -1) // center on the display
     {
       iOffX = (GIF_DISPLAY_WIDTH - gif.getCanvasWidth()) / 2;
       iOffY = (GIF_DISPLAY_HEIGHT - gif.getCanvasHeight()) / 2;
@@ -177,7 +187,89 @@ bool DisplayGIF(uint8_t *imageData, int imageSize, uint8_t numberRepetitons, int
 // ======================================================================================================
 // ======================================================================================================
 
-void initGIF(void)
+#else // not GIF_DIRECT_RENDER_TO_LCD
+
+// RENDER TO MEMORY -> RESIZE -> TFT DISPLAY
+// Code is from the example:
+// https://github.com/bitbank2/AnimatedGIF/blob/master/examples/big_mem_demo/big_mem_demo.ino
+
+// The code for MCUs with enough RAM is much simpler because the AnimatedGIF library can
+// generate "cooked" pixels that are ready to send to the display
+
+// Draw a line of image into the buffer
+void GIFrenderToLCD_callback(GIFDRAW *pDraw)
 {
-  gif.begin(BIG_ENDIAN_PIXELS);
+  if (pDraw->y == 0)
+  { // set the memory window when the first line is rendered
+    tft.setAddrWindow(pDraw->iX, pDraw->iY, pDraw->iWidth, pDraw->iHeight);
+  }
+  // For all other lines, just push the pixels to the display
+  tft.pushPixels((uint8_t *)pDraw->pPixels, pDraw->iWidth * 2);
 }
+
+// memory allocation callback function
+void *GIFAlloc(uint32_t u32Size)
+{
+  return malloc(u32Size);
+}
+
+// memory free callback function
+void GIFFree(void *p)
+{
+  free(p);
+}
+
+/*
+#define iCanvasSize  (GIF_IMG_WIDTH * (GIF_IMG_HEIGHT + 3))
+__attribute__((section(".ext_ram.bss")))  uint8_t imageStaticCanvas[iCanvasSize];
+*/
+
+bool DisplayGIF(uint8_t *imageData, int imageSize, uint8_t numberRepetitons, int positionX, int positionY)
+{
+  bool ok = false;
+  bool result = false;
+  int rr = 0;
+  if (gif.open((uint8_t *)imageData, imageSize, GIFrenderToLCD_callback))
+  {
+    Serial.printf("Successfully opened GIF; Canvas size = %d x %d\n", gif.getCanvasWidth(), gif.getCanvasHeight());
+    // Allocate an internal buffer to hold the full canvas size
+
+#ifdef iCanvasSize
+    gif.setFrameBuf(imageStaticCanvas);
+    ok = (gif.getCanvasWidth() == GIF_IMG_WIDTH) && (gif.getCanvasHeight() == GIF_IMG_HEIGHT);
+#else
+    ok = (gif.allocFrameBuf(GIFAlloc) == GIF_SUCCESS);
+#endif
+
+    if (ok)
+    {
+      gif.setDrawType(GIF_DRAW_COOKED); // we want the library to generate ready-made pixels
+      tft.startWrite();                 // The TFT chip select is locked low
+      for (uint8_t i = 0; i < numberRepetitons; i++)
+      {
+        // it automatically restarts the gif from the beginning
+        while (rr = gif.playFrame(true, NULL))
+        {
+          yield();
+        }
+        if (rr >= 0)
+        {
+          result = true;
+        }
+      }
+      gif.close();
+      tft.endWrite(); // Release TFT chip select for other SPI devices
+#ifndef iCanvasSize
+      gif.freeFrameBuf(GIFFree);
+#endif
+    }
+    else
+    {
+      Serial.println("Insufficient memory!");
+    }
+    gif.close();
+  }
+  return result;
+} // DisplayGIF()
+
+#endif // GIF_DIRECT_RENDER_TO_LCD
--- src/GIFDraw.h
+++ src/GIFDraw.h
@@ -2,12 +2,15 @@
 
 //#define GIF_SPEED_TEST
 
+#define GIF_DIRECT_RENDER_TO_LCD
+#define GIF_SCALING      1
+
+#define GIF_IMG_WIDTH    220
+#define GIF_IMG_HEIGHT   208
 
 #define GIF_DISPLAY_WIDTH  DspW
 #define GIF_DISPLAY_HEIGHT DspH
-#define GIF_BUFFER_SIZE 256            // Optimum is >= GIF width or integral division of width
-
-//void GIFrenderToLCD_callback(GIFDRAW *pDraw);
+#define GIF_LINE_BUFFER_SIZE  GIF_IMG_WIDTH            // Optimum is >= GIF width or integral division of width
 
 bool DisplayGIF(uint8_t *imageData, int imageSize, uint8_t numberRepetitons, int positionX = -1, int positionY = -1);
 void initGIF(void);
